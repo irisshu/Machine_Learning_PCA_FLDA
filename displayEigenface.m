baa2dcbb915360b1bec1bd7e53ed31fdd45852d6
irisshu <irishsuyt@gmail.com>
2014-11-07 22:45:49 +0800

    Compute the centered data matrix and show image.

--- displayEigenface.m
+++ displayEigenface.m
@@ -2,11 +2,19 @@
 pid = 1;
 NumImgs = 21;
 photo = double(imread('PIE_Nolight/1/1.bmp'));
-X = zeros([NumImgs size(photo)]);
+imgMatrix = zeros(100*100, NumImgs);
+
 
     for i = 1:NumImgs
         path = ['PIE_Nolight/' num2str(pid) '/' num2str(i) '.bmp'];
         photo = imread(path);
-        %X(i,:,:) = photo;
         columnVec = photo(:);
+        imgMatrix(: ,i) = columnVec;
+        
     end
+meanVal =  mean(imgMatrix,2);
+for i=1:NumImgs
+    imgMatrix(: ,i) = imgMatrix(: ,i)-meanVal;
+    subplot(5,5,i);
+    image(reshape(imgMatrix(:,i),[100,100]));
+end
