58cce5a2015696c1c78a034875ef5757e422650a
Iris Hsu <irishsuyt@gmail.com>
2014-11-09 16:56:55 +0800

    Train the set of eigenface from training data, and get V, m, D.

---
+++ recog_pca.m
@@ -0,0 +1,32 @@
+
+% Only calculate PCA for one person ----------------------------
+NumTrain = 3;
+% NumPeople = 65;
+imgMatrix = zeros(100*100, NumTrain);
+trainIndex = {7,10,19};
+
+for i=1:NumTrain
+    path = ['PIE_Nolight/1/' num2str(cell2mat(trainIndex(i))) '.bmp'];
+    photo = imread(path);
+    columnVec = photo(:);
+    imgMatrix(: ,i) = columnVec;  
+end   
+
+m =  mean(imgMatrix,2); % Get mean vector m
+for i=1:NumTrain
+    imgMatrix(: ,i) = imgMatrix(: ,i)-m;  
+end
+
+G = imgMatrix'*imgMatrix;
+[u, oD] = eig(G);
+v = imgMatrix*u;
+v = (v/norm(v));
+
+dvec = diag(oD);
+V = zeros(size(v));
+[dvec,index_dv] = sort(abs(dvec));
+index_dv = flipud(index_dv);
+for i = 1:size(oD,1)
+  D(i,1) = oD(index_dv(i),index_dv(i)); % Get column vector D (eigenvalue)
+  V(:,i) = v(:,index_dv(i)); % Get matrix V
+end;
