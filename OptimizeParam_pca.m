46b2c0cbd9529ce3b1a5f700e4b753d3ecb15200
irisshu <irishsuyt@gmail.com>
2014-11-11 17:28:47 +0800

    Use global PCA.

--- OptimizeParam_pca.m
+++ OptimizeParam_pca.m
@@ -1,8 +1,5 @@
- function recogRate = OptimizeParam_pca(k)
- subDim = k;
-% If subDim is not given, subDim = dim - 1;
-
-%subDim = 195-1;
+function recogRate = Opt_test(k)
+subDim = k;
 
 NumTrain = 3;
 NumPeople = 65;
@@ -10,95 +7,88 @@ imgMatrix = zeros(100*100, NumTrain);
 trainIndex = {7,10,19};
 
 
-% Creating training images space
-dim = NumPeople*NumTrain ;
 for i=1:NumPeople
     for j=1:NumTrain
         path = ['PIE_Nolight/' num2str(i) '/' num2str(cell2mat(trainIndex(j))) '.bmp'];
         photo = imread(path);
         columnVec = photo(:);
-        imgMatrix(: ,i*j) = columnVec;  
+        imgMatrix(: ,j) = columnVec;  
     end   
-end
-    % Get mean vector m
-    m =  mean(imgMatrix,2); 
-    for i=1:dim
-        imgMatrix(: ,i) = imgMatrix(: ,i)-m;  
+
+    m =  mean(imgMatrix,2); % Get mean vector m
+    for j=1:NumTrain
+        imgMatrix(: ,j) = imgMatrix(: ,j)-m;  
     end
 
-    %PCA
     G = imgMatrix'*imgMatrix;
     [u, oD] = eig(G);
-    dvec = diag(oD);        
+    v = imgMatrix*u;
+    v = (v/norm(v));
+
+    dvec = diag(oD);
+    V = zeros(size(v));
+    D = zeros(size(oD,1));
+        
     [dvec,index_dv] = sort(abs(dvec));
     index_dv = flipud(index_dv);
-    
-    D = zeros(size(oD));
-    V = zeros(size(u));
-    for i = 1:size(oD,1)
-      D(i,i) = oD(index_dv(i),index_dv(i)); % Get column vector D (eigenvalue)
-      V(:,i) = u(:,index_dv(i)); % Get matrix V
+    for j = 1:size(oD,1)
+      D(j,1) = oD(index_dv(j),index_dv(j)); % Get column vector D (eigenvalue)
+      V(:,j) = v(:,index_dv(j)); % Get matrix V
     end;
+   
+    for j=1:NumTrain
+        path = ['PIE_Nolight/' num2str(i) '/' num2str(cell2mat(trainIndex(j))) '.bmp'];
+        photo = imread(path);
+        p = double(photo(:));
+        pc_train{i}{j} = V'*(p-m); 
+    end   
+end
 
-    D = diag(D);
-    D = D / (dim-1);
-    D = D(1 : subDim);        % Retaining only the largest subDim ones
-
-    V = imgMatrix * V;    % Turk-Pentland trick (part 2)
-    
-    % Normalisation to unit length
-    for i = 1 : dim
-        V(:, i) = V(:, i) / norm(V(:, i));
-    end;
-    
-    % Dimensionality reduction. 
-    w = V(:, 1:subDim);
-    
-   % Subtract mean face from all images 
-    %zeroMeanDATA = zeros(size(DATA));
-    for i=1:NumPeople
-        for j=1:NumTrain
-            path = ['PIE_Nolight/' num2str(i) '/' num2str(cell2mat(trainIndex(j))) '.bmp'];
-            photo = imread(path);
-            p = double(photo(:));
-            pc_train{i}{j} = w'*(p-m); 
-        end   
-    end
-
-
-%---------------
 correct_ans = 0;
-    
-for i=1:NumPeople
+
+class_Label = zeros(65,65);    
+for i=1:NumPeople   
     for j=1:21
        if(j~=7 && j~=10 && j~= 19)
             % Calculate pc_test
             path = ['PIE_Nolight/' num2str(i) '/' num2str(j) '.bmp'];
             photo = imread(path);
             p = double(photo(:));
-            pc_test = w'*(p-m);               
+            pc_test = V'*(p-m);               
 
-            %label  
-            minDist = inf;    
+            %label               
             for c=1:65
-                for q=1:3
-                     Dist = norm(pc_test-pc_train{c}{q});
-                    if (Dist < minDist)
-                        minDist = Dist;
-                        classLabel = c;                          
-                    end                   
-                end               
+                for k=1:3
+                     Dist(3*(c-1) + k) = norm(pc_test-pc_train{c}{k});
+                end
             end
-            if(classLabel==i)
-                   correct_ans = correct_ans +1;
-            end   
+            [Dist,index] = sort(Dist);
+                        
+             
+           
+            for ii=1 :subDim
+                q = index(ii)/3;
+                q = floor(q);
+                m_od = mod(index(ii),3);
+                %ii = ii +1;
+                if (m_od~=0)
+                    q = q+1;                    
+                end
+                class_Label(q,i) = class_Label(q,i)+1;
+                if(class_Label(q,i) ==3)
+                    ii =1000; % ¸õ¥X°j°é
+                end
+            end
+            if(q==i)
+               correct_ans = correct_ans +1;
+            end
         end        
     end      
 end
 
 recogRate = correct_ans/(65*(21-3)) ;
-line = ['Recognition rate = ',num2str(recogRate) ];
+recog = correct_ans/(NumPeople*(21-3)) *100;
+line = ['Recognition rate = ',num2str(recog), '%' ];
 disp(line);
-
-
 end
+
